3a801f1835877c7f2c20059f714a2f8d58e29774
Kendall McGree <mcgreekendall@gmail.com>
2022-03-05 09:55:12 -0800

    Add Emojis For Weapon Selection

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-var game = new Game();
+// var game = new Game();
+
+//VARIABLES/QUERY SELECTORS
+var classicGame = document.querySelector(".easy-mode");
+var modernGame = document.querySelector(".hard-mode");
+var classicWeapons = document.querySelector(".classic-weapons");
+var rock = document.querySelector(".rock-emoji");
+var paper = document.querySelector(".paper-emoji");
+var scissors = document.querySelector(".scissors-emoji");
+// var modernWeapons = document.querySelector(".modern-weapons");
+
+//GLOBAL VARIABLES
+
+//EVENT LISTENERS
+classicGame.addEventListener("click", showClassicWeapons);
+rock.addEventListener("click", pickRock);
+paper.addEventListener("click", pickPaper);
+scissors.addEventListener("click", pickScissors);
+// classicGame.addEventListener("click", showModernWeapons);
+
+//FUNCTIONS
+function showClassicWeapons() {
+  classicWeapons.classList.remove("hidden");
+  classicGame.classList.add("hidden");
+  modernGame.classList.add("hidden");
+}
+
+function pickRock() {
+  Player.weapon = "rock";
+  console.log(Player.weapon);
+  //   Game.declareWinner();
+}
+
+function pickPaper() {
+  Player.weapon = "paper";
+  console.log(Player.weapon);
+}
+
+function pickScissors() {
+  Player.weapon = "scissors";
+  console.log(Player.weapon);
+}
+// function showModernWeapons() {
+//   modernWeapons.classList.remove("hidden");
+//   classicGame.classList.add("hidden");
+//   modernGame.classList.add("hidden");
+// }
