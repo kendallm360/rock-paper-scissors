85de7c6ed57cb9ca41b515aed8a5b7b61b2a59ce
Kendall McGree <mcgreekendall@gmail.com>
2022-03-06 18:35:26 -0800

    Add Game Board and Data Model to DOM

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
+//GLOBAL VARIABLES
 var game = new Game();
 
 //VARIABLES/QUERY SELECTORS
 var changeGame = document.querySelector(".change-game");
 var chooseGame = document.querySelector(".choose-game");
 var chooseWeapon = document.querySelector(".choose-weapon");
-// var winnerAlert = document.querySelector(".winner-alert");
 var classicGame = document.querySelector(".classic-mode");
 var modernGame = document.querySelector(".modern-mode");
 var classicWeapons = document.querySelector(".classic-weapons");
-// var modernWeapons = document.querySelector(".modern-weapons");
-// var classicRock = document.querySelector(".classic-rock-emoji");
-
 var rock = document.querySelector(".rock-emoji");
 var paper = document.querySelector(".paper-emoji");
 var scissors = document.querySelector(".scissors-emoji");
@@ -18,23 +15,18 @@ var hunter = document.querySelector(".hunter-emoji");
 var ninja = document.querySelector(".ninja-emoji");
 var winnerBox = document.querySelector(".winner-box");
 var winnerAlert = document.querySelector(".winner-alert");
-
-//GLOBAL VARIABLES
+var humanWins = document.querySelector(".human-wins");
+var computerWins = document.querySelector(".computer-wins");
 
 //EVENT LISTENERS
 classicGame.addEventListener("click", showClassicWeapons);
-
 modernGame.addEventListener("click", showModernWeapons);
-
 changeGame.addEventListener("click", newGame);
-// modernRock.addEventListener("click", pickModernRock);
-// classicWeapons.addEventListener("click", pickWeapon2);
 rock.addEventListener("click", pickRock);
 paper.addEventListener("click", pickPaper);
 scissors.addEventListener("click", pickScissors);
 hunter.addEventListener("click", pickHunter);
 ninja.addEventListener("click", pickNinja);
-// winnerBox.addEventListener("click", displayWinner);
 
 //FUNCTIONS
 
@@ -57,7 +49,6 @@ function showClassicWeapons() {
 
 function showModernWeapons() {
   game.startNewGame("modern");
-
   changeGame.classList.remove("hidden");
   chooseGame.classList.add("hidden");
   chooseWeapon.classList.remove("hidden");
@@ -90,9 +81,11 @@ function pickRock() {
     game.declareModernWinner("rock");
   }
   if (game.winner === "human") {
+    humanWins.innerText += game.player.wins;
     winnerAlert.innerText = "The Human Wins This Round";
   }
   if (game.winner === "computer") {
+    computerWins.innerText += game.computerWins;
     winnerAlert.innerText = "The Computer Wins This Round";
   }
   if (game.winner === "draw") {
@@ -182,9 +175,9 @@ function displayWinner() {
   classicGame.classList.add("hidden");
   modernGame.classList.add("hidden");
   winnerAlert.classList.remove("hidden");
-
+  //NEED TO CONNECT EMOJI TO THE LINE BELOW INSTEAD OF THE TEXT
+  winnerBox.innerText = game.player.weapon + " EMOJI WILL BE HERE";
   setTimeout(function () {
-    //everything here will be what shows after reset
     if (game.gameMode === "modern") {
       showModernWeapons();
     } else {
@@ -192,35 +185,3 @@ function displayWinner() {
     }
   }, 2000);
 }
-
-// save for refactoring
-// function pickWeapon(weapon) {
-//   if (this.gameMode === "classic") {
-//     game.declareClassicWinner(weapon);
-//   } else {
-//     game.declareModernWinner(weapon);
-//   }
-// }
-
-//variation 2 below
-// function pickWeapon2(event) {
-//   if (this.gameMode === "classic") {
-//     game.declareClassicWinner(event.target.value);
-//   } else {
-//     game.declareModernWinner(event.target.value);
-//   }
-// }
-
-//save for refactor
-// function handlePlayerChoice(event) {
-//   if (game.gameMode === "classic") {
-//     handlePlayerChoiceClassic(event);
-//   }
-// }
-// function handlePlayerChoiceClassic(event) {
-//   if (event.target.classList.contains(".rock-emoji")) {
-//     pickWeapon(event.target.value);
-//   } else if (event.target.classList.contains(".paper-emoji")) {
-//     pickWeapon("paper");
-//   }
-// }
